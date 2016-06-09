cff9317fbf4e15d9c66b4720d3e664d5dca358ce
Oliver Seibert <oliverseibert@hotmail.de>
2016-06-09 12:35:05 +0200

    Funktionen zur Kalkulation der Priorität implementiert + Trigger zum aktualisieren dieser Prio nach einem Insert oder Update

---
+++ Datenbank/Stored Procedures/tweets_helper.sql
@@ -0,0 +1,51 @@
+/* Implementiert die Formel zur Priorisieren von Tweets
+	Rückgabe: Prio als float
+*/
+DELIMITER $$
+drop function if exists get_prio;
+create function get_prio(
+	p_likes int, p_retweets int, p_follower int
+) returns float
+begin
+	declare l_prio float default 0;
+	
+	if p_follower is not null then
+		set l_prio = round((((ifnull(p_likes,0) / 2) + ifnull(p_retweets,0) * 2) / ifnull(p_follower,1)) * 100, 3);
+	end if;
+	
+	return l_prio;
+end;$$
+
+/* Errechnet die Priorität für einen Tweet anhand dessen Retweets, Likes und Anzahl Follower
+   Rückgabe: Prio als float
+*/
+DELIMITER $$
+drop function if exists calc_tweet_prio;
+create function calc_tweet_prio(
+	p_tweet_id bigint
+) returns float
+begin
+	declare l_prio float default 0;
+	
+	select get_prio(t.anzahl_likes, t.anzahl_retweets, a.anzahl_follower)
+		into l_prio
+		from tweets t, tweet_autor a
+		where t.autor_id = a.autor_id
+		and t.tweet_id = p_tweet_id;
+
+	return l_prio;
+end;$$
+
+/* Setzt die Tweet-Priorität neu
+*/
+DELIMITER $$
+drop procedure if exists recalc_tweet_prio;
+create procedure recalc_tweet_prio(
+	p_tweet_id bigint
+) 
+begin
+	declare l_prio float default 0;
+	set l_prio = calc_tweet_prio(p_tweet_id);
+	
+	update tweets set prio_calc = l_prio where tweet_id = p_tweet_id;
+end;$$
--- Datenbank/Trigger/tweets.sql
+++ Datenbank/Trigger/tweets.sql
@@ -1,5 +1,6 @@
 /* Trigger gleicht nach dem Einfügen eines neuen Tweets
-   	die Keywords mit dem neuen Tweet ab
+   	die Keywords mit dem neuen Tweet ab und
+	setzt die Priorität
 */
 DELIMITER $$
 drop trigger if exists tweets_after_insert;
@@ -11,4 +12,25 @@ begin
 		(select distinct k.keyword, t.tweet_id from tweets t, keywords k
 			where new.text regexp k.keyword
 			and t.tweet_id = new.tweet_id);
+	call recalc_tweet_prio(new.tweet_id);
+end;$$
+
+/* Trigger errechnet nach dem Ändern der Spalten Likes oder Retweets eines Tweets
+   	die Priorität neu
+*/
+DELIMITER $$
+drop trigger if exists tweets_before_update;
+create trigger tweets_before_update
+before update
+	on tweets for each row
+begin
+	declare l_anzahl_follower int;
+	
+	if new.anzahl_likes <=> old.anzahl_likes or new.anzahl_retweets <=> old.anzahl_retweets then
+		select anzahl_follower into l_anzahl_follower
+			from tweet_autor
+			where autor_id = new.autor_id;
+			
+		set new.prio_calc = get_prio(new.anzahl_likes, new.anzahl_retweets, l_anzahl_follower);
+	end if;
 end;$$
