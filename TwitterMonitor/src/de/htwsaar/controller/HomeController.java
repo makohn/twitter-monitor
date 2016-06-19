cdd51dab0e27a19e6757c8710ffaa0216567182b
makohn <marek.kohn@gmx.net>
2016-06-19 21:41:47 +0200

    Frontend-Anbindung für Benutzerverwaltung hinzugefügt

--- TwitterMonitor/src/de/htwsaar/controller/HomeController.java
+++ TwitterMonitor/src/de/htwsaar/controller/HomeController.java
@@ -1,16 +1,35 @@
 
 package de.htwsaar.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import de.htwsaar.model.User;
+import de.htwsaar.services.user.UserService;
 
 @Controller
 public class HomeController {
 
+	private UserService userService;
+	
+	@Autowired
+	public void serUsersService(UserService userService) {
+		this.userService = userService;
+	}
+	
 	@RequestMapping("/")
-	public String showHome(Model testModel){
-		
+	public String showHome(Model model){
+		model.addAttribute("user", new User());
 		return "home";
+	}
+	
+	@RequestMapping(value="/newAccount", method=RequestMethod.POST) 
+	public String newAccount(User user){
+		userService.insertUser(user);
+		
+		return "showTweets";	
 	}
 }
