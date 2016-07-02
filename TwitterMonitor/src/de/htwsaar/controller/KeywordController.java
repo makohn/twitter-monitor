7969de9cd9160851fd03b57e28bbebf2f8083b05
makohn <marek.kohn@gmx.net>
2016-07-02 12:11:14 +0200

    Keywordverwaltung hinzugefuegt
    - Keyword
    - KeywordDao
    - KeywordController
    - Methode: getKeywords in UserService
    
    Es wurde ein Keyword-Controller hinzugefuegt, der die Keywords eines
    Users aus der Datenbank lädt und als JSON an das Frontend weitergibt.
    
    Dazu benoetigt er eine Methode im User Service, welche auf die neu
    hinzugefuegte Keyword DAO zugreift und Objekte vom Typ 'Keyword'
    erstellt.

---
+++ TwitterMonitor/src/de/htwsaar/controller/KeywordController.java
@@ -0,0 +1,46 @@
+package de.htwsaar.controller;
+
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import de.htwsaar.model.Keyword;
+import de.htwsaar.service.user.UserService;
+
+@Controller
+public class KeywordController {
+
+	private UserService userService;
+
+	@Autowired
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@RequestMapping("/keywords")
+	public String loadKeywords(Model model) {
+		//model.addAttribute("keyword", new Keyword());
+		return "keywords";
+	}
+
+	@RequestMapping(value = "getKeywords", method = RequestMethod.GET, headers = "Accept=application/json")
+	@ResponseBody
+	public Map<String, Object> getKeywords(Principal principal) {
+		
+		String username = principal.getName();
+		
+		List<Keyword> keywords = userService.getKeywords(username);
+		Map<String, Object> data = new HashMap<String, Object>();
+		data.put("keywords", keywords);
+
+		return data;
+	}
+}
