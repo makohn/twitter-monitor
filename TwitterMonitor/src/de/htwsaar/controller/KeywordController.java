f927e1b790ef9e92ef8406ecf5e79578c5273666
makohn <marek.kohn@gmx.net>
2016-07-02 14:07:20 +0200

    updatePriority Methode im KeywordController hinzugefuegt

--- TwitterMonitor/src/de/htwsaar/controller/KeywordController.java
+++ TwitterMonitor/src/de/htwsaar/controller/KeywordController.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -42,5 +45,14 @@ public class KeywordController {
 		data.put("keywords", keywords);
 
 		return data;
+	}
+	
+	@RequestMapping(value = "changePriority", method = RequestMethod.POST) 
+	public @ResponseBody String changePriority(@RequestBody Keyword updateKeyword, HttpServletRequest request) {
+		String keyword = updateKeyword.getKeyword();
+		int priority = updateKeyword.getPriority();
+		
+		
+		return "keywords";
 	}
 }
