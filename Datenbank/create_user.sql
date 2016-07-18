bc2d71bfb354fe16319105f27ae83fd31801738f
Oliver Seibert <oliverseibert@hotmail.de>
2016-07-18 16:16:53 +0200

    Der DB-Nutzer "twitteruser" darf auch die keywords löschen

--- Datenbank/create_user.sql
+++ Datenbank/create_user.sql
@@ -6,7 +6,7 @@ grant select on twitter_monitor.* to 'twitteruser';
 grant update, insert on twitter_monitor.users to 'twitteruser';
 grant update, insert on twitter_monitor.authorities to 'twitteruser';
 grant update, insert on twitter_monitor.user_x_preferences to 'twitteruser';
-grant update, insert on twitter_monitor.keywords to 'twitteruser';
+grant update, insert, delete on twitter_monitor.keywords to 'twitteruser';
 grant update, insert on twitter_monitor.tweetAuthors to 'twitteruser';
 grant update, insert on twitter_monitor.tweets to 'twitteruser';
 grant update, insert on twitter_monitor.tweetMedia to 'twitteruser';
