5a96ac723044460c3a71fbb113a83fc56d2a4692
philipp <philipp@Achilles>
2016-08-31 16:27:59 +0200

    Schönheitsfehler

--- TwitterMonitor/src/de/htwsaar/exception/TwitterMonitorException.java
+++ TwitterMonitor/src/de/htwsaar/exception/TwitterMonitorException.java
@@ -3,15 +3,16 @@ package de.htwsaar.exception;
 import de.htwsaar.util.TweetLogger;
 
 public class TwitterMonitorException extends Exception {
-	
+
 	private static final long serialVersionUID = 2093045877934395643L;
 
+	private static final String MSG = "Unbekannte TwitterMonitorException";
+
 	public TwitterMonitorException() {
-		TweetLogger.insertLog("Unbekannte TwitterMonitorException");
+		super(MSG);
 	}
-	
+
 	public TwitterMonitorException(String msg) {
-		TweetLogger.insertLog(msg);
+		super(msg);
 	}
 }
-
