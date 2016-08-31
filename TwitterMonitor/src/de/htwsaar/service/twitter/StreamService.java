f1cacd8b591a705128788e3cafce626f39e7b828
philipp <philipp@Achilles>
2016-08-31 16:41:43 +0200

    Neustart des Streams nur bei neuen Keywords

--- TwitterMonitor/src/de/htwsaar/service/twitter/StreamService.java
+++ TwitterMonitor/src/de/htwsaar/service/twitter/StreamService.java
@@ -11,21 +11,24 @@ import twitter4j.TwitterStreamFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
- * The class StreamService is responsible for creating a stream to the Twitter APi as well as
- * a filter, which configures and initializes the stream which defined parameters, such as
- * keywords.
+ * The class StreamService is responsible for creating a stream to the Twitter
+ * APi as well as a filter, which configures and initializes the stream which
+ * defined parameters, such as keywords.
  *
  * @author Philipp Schaefer, Moritz Grill
  */
 
 @Service
 public class StreamService {
-	
+
 	private static final String CONSUMER_KEY = "4y9HVRAg43m3dfWoDKCWOzf9x";
 	private static final String CONSUMER_SECRET = "GdZVRXMaGYn2b4PTXficnQVztCbE8eSlBJPT2zIIY5xn45zZRt";
 	private static final String ACCESS_TOKEN = "712907200507850753-BNhxmqynkH6R7LyxG4GUsOf6pGP9i2L";
 	private static final String ACCESS_TOKEN_SECRET = "xPLvu603NO1l1GJzZtmUNNokKqsdj1obVhrVHsHNAa0l8";
-//	private static final long FIFTEEN_MINUTES = 15 * 60 * 1000;;
+
+	private static final long ONE_MINUTE = 1 * 60 * 1000;
+
+	private String[] keywordsArray;
 
 	private TwitterStream stream;
 	private TweetListener tweetListener;
@@ -35,13 +38,15 @@ public class StreamService {
 	public StreamService(TweetListener tweetListener, KeywordDao keywordDao) {
 		this.tweetListener = tweetListener;
 		this.keywordDao = keywordDao;
-		
+
+		keywordsArray = keywordDao.getKeywords();
+
 		startStream();
 	}
 
 	/**
-	 *  This method builds a stream to the twitter API by using the
-	 *  OAuth Credentials that are connected to this application.
+	 * This method builds a stream to the twitter API by using the OAuth
+	 * Credentials that are connected to this application.
 	 */
 	private void createStream() {
 		ConfigurationBuilder cb = new ConfigurationBuilder();
@@ -56,33 +61,22 @@ public class StreamService {
 	}
 
 	/**
-	 * This method initializes the stream with keywords and other filters,
-	 * such as languages. 
+	 * This method initializes the stream with keywords and other filters, such
+	 * as languages.
 	 */
 	private void initStream() {
 
-//		String[] keywordsArray = { "Wasser", "Deutschland", "Hamburg", "Berlin", "Paris", "America", "Trump",
-//				"Clinton" };
-//		// Erstelle Filter ..
-//		FilterQuery filter = new FilterQuery();
-//		filter.track(keywordsArray);
-//		filter.language("de", "en");
-//		// weitere Konfigurationen ...
-//		stream.filter(filter);
+		if ((keywordsArray != null) && (keywordsArray.length != 0)) {
 
-		String[] keywordsArray = keywordDao.getKeywords();
-		if ( (keywordsArray != null) && (keywordsArray.length != 0) ) {
-		
 			// Erstelle Filter ..
 			FilterQuery filter = new FilterQuery();
 			filter.track(keywordsArray);
 			filter.language("de", "en");
 
 			// weitere Konfigurationen ...
-		
+
 			stream.filter(filter);
 		}
-
 	}
 
 	public void startStream() {
@@ -95,11 +89,31 @@ public class StreamService {
 		stream.shutdown();
 	}
 
-//	@Scheduled(cron = "0 1 * * * ?")
-	@Scheduled(fixedDelay = /*FIFTEEN_MINUTES*/3*60*1000)
+	@Scheduled(fixedDelay = ONE_MINUTE)
 	public void restartStream() {
-		System.out.println("restart Stream");
-		stopStream();
-		startStream();
+
+		String[] newKeywordsArray = keywordDao.getKeywords();
+
+		if (keywordsChanged(keywordsArray, newKeywordsArray)) {
+			keywordsArray = newKeywordsArray;
+			stopStream();
+			startStream();
+		}
+	}
+
+	private boolean keywordsChanged(String[] oldKeywords, String[] newKeywords) {
+
+		boolean changed = false;		
+		if (oldKeywords.length != newKeywords.length) {
+			changed = true;
+		} else {
+			for (int i = 0; i < newKeywords.length; i++) {
+				if (!newKeywords[i].equals(oldKeywords[i])) {
+					changed = true;
+					break;
+				}
+			}
+		}
+		return changed;
 	}
 }
