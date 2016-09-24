f52df0aae105993d813f4b089969ebcd45f14705
makohn <marek.kohn@gmx.net>
2016-09-24 11:14:40 +0200

    NotificationJobManager in NotificationService integriert

--- TwitterMonitor/src/de/htwsaar/service/user/NotificationService.java
+++ TwitterMonitor/src/de/htwsaar/service/user/NotificationService.java
@@ -3,10 +3,12 @@ package de.htwsaar.service.user;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import de.htwsaar.db.NotificationDao;
 import de.htwsaar.model.Notification;
+import de.htwsaar.util.MailSender;
 
 /**
  * Service zum Verwalten der Benachrichtigungen
@@ -15,6 +17,9 @@ import de.htwsaar.model.Notification;
  */
 @Service
 public class NotificationService {
+	private static final String TYPE_EMAIL = "ema";
+	private static final int FIVE_MINUTES = 5*60*1000;
+	
 	private NotificationDao notificationDao;
 	
 	@Autowired
@@ -28,5 +33,26 @@ public class NotificationService {
 	
 	public void updateNotificationSent(Notification notification){
 		notificationDao.updateNotificationSent(notification);
+	}
+	
+	/**
+	 * Versendet eine Benachrichtigung und markiert sie entsprechend als versendet
+	 */
+	@Scheduled(fixedDelay = FIVE_MINUTES)
+	private void sendNotifications() {
+		List<Notification> notificationList = getNotificationsNotSent();
+
+		for (Notification notification : notificationList) {
+			System.out.println("Versende: " + notification);
+			switch (notification.getType()) {
+			case TYPE_EMAIL:
+				MailSender.sendMail(notification.getEmail(), notification.getSubject(), notification.getBody());
+				updateNotificationSent(notification);
+				break;
+			default:
+				System.out.println("Notification-Type " + notification.getType() + " wird nicht unterstützt");
+				break;
+			}
+		}		
 	}
 }
